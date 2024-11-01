4bacad2e08ca8d9bba37fc3aa1bbc8b999cc076d
James Cracknell <james.h.cracknell@gmail.com>
2024-10-31 21:36:11 -0600

    Use SmallDictionary for single expression replacement

--- src/Arborist/src/ExpressionHelpers.Replace.cs
+++ src/Arborist/src/ExpressionHelpers.Replace.cs
@@ -1,5 +1,5 @@
 using Arborist.Internal;
-using System.Collections.Immutable;
+using Arborist.Internal.Collections;
 
 namespace Arborist;
 
@@ -9,8 +9,7 @@ public static partial class ExpressionHelpers {
     /// <paramref name="replacement"/> expression in the subject <paramref name="expression"/>.
     /// </summary>
     public static Expression Replace(Expression expression, Expression search, Expression replacement) =>
-        // TODO: Could potentially add an optimized single-element IReadOnlyDictionary implementation for this case
-        Replace(expression, ImmutableDictionary<Expression, Expression>.Empty.Add(search, replacement));
+        Replace(expression, SmallDictionary.Create(KeyValuePair.Create(search, replacement)));
 
     /// <summary>
     /// Replaces all occurrences of the expressions identified by the provided <paramref name="replacements"/> mapping
@@ -18,21 +17,4 @@ public static partial class ExpressionHelpers {
     /// </summary>
     public static Expression Replace(Expression expression, IReadOnlyDictionary<Expression, Expression> replacements) =>
         new ReplacingExpressionVisitor(replacements).Visit(expression);
-
-    private static IReadOnlyDictionary<Expression, Expression> CreateReplacementsDictionary(
-        IEnumerable<KeyValuePair<Expression, Expression>> replacements
-    ) {
-        // If you are manually stitching together a large number of expression trees, it is common to end up with
-        // duplicated ParameterExpression instances, so we have to build up the replacements dictionary via assignment.
-        var count = replacements.TryGetNonEnumeratedCount(out var n) ? n : 0;
-        var dictionary = new Dictionary<Expression, Expression>(0);
-        foreach(var (search, replace) in replacements) {
-            if(dictionary.TryGetValue(search, out var existing) && !replace.Equals(existing))
-                throw new InvalidOperationException($"Provided collection of replacement expressions contains multiple replacements for search expression {search}.");
-
-            dictionary[search] = replace;
-        }
-
-        return dictionary;
-    }
 }

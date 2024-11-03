0f5feec0f863d9379767d66804eab16d98b5bcaf
James Cracknell <james.h.cracknell@gmail.com>
2024-11-03 13:28:27 -0700

    Add internal FunctionHelpers

---
+++ src/Arborist/src/Internal/FunctionHelpers.cs
@@ -0,0 +1,15 @@
+namespace Arborist.Internal;
+
+public static class FunctionHelpers {
+    /// <summary>
+    /// The identity function. Returns its input value verbatim.
+    /// </summary>
+    public static A Identity<A>(A value) =>
+        value;
+
+    /// <summary>
+    /// Immediately executes the provided <paramref name="thunk"/>, returning its result.
+    /// </summary>
+    public static A Immediate<A>(Func<A> thunk) =>
+        thunk();
+}

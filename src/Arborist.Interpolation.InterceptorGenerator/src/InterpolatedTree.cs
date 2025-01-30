2aa8d2875100294fb74dfe75c3d3fe7da8a3a6c1
James Cracknell <james.h.cracknell@gmail.com>
2025-01-30 10:58:20 -0400

    Add ! as a preallocated verbatim InterpolatedTree

--- src/Arborist.Interpolation.InterceptorGenerator/src/InterpolatedTree.cs
+++ src/Arborist.Interpolation.InterceptorGenerator/src/InterpolatedTree.cs
@@ -5,6 +5,7 @@ public abstract class InterpolatedTree : IEquatable<InterpolatedTree> {
         new[] {
             "",
             ".",
+            "!",
             ",", ", ",
             "<", ">",
             "(", ")",

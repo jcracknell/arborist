c23ff5c1087e8c7a8dd7fe3cab528cee91a92e7e
James Cracknell <james.h.cracknell@gmail.com>
2025-02-10 14:35:49 -0400

    Add test cases for SpliceBody applied to thunks

--- src/Arborist.Interpolation.InterceptorGenerator/test/SpliceBodyTests.cs
+++ src/Arborist.Interpolation.InterceptorGenerator/test/SpliceBodyTests.cs
@@ -2,10 +2,75 @@ namespace Arborist.Interpolation.InterceptorGenerator;
 
 public class SpliceBodyTests {
     [Fact]
+    public void Should_work_for_Func1_provided_via_data() {
+        // This test case is necessary because the call to replace is no longer required (and you cannot rely
+        // on type inferral in the SmallDictionary construction)
+        var results = InterpolationInterceptorGeneratorTestBuilder.Create()
+        .Generate(@"
+            ExpressionOnNone.Interpolate(
+                new { Thunk = ExpressionOnNone.Of(() => 41) },
+                x => x.SpliceBody(x.Data.Thunk) + 1
+            );
+        ");
+        
+        Assert.Equal(1, results.AnalysisResults.Count);
+        CodeGenAssert.CodeEqual(
+            expected: @"
+                (global::System.Linq.Expressions.BinaryExpression)(expression.Body) switch {
+                    var __e0 => global::System.Linq.Expressions.Expression.MakeBinary(
+                        __e0.NodeType,
+                        (global::System.Linq.Expressions.MethodCallExpression)(__e0.Left) switch {
+                            var __e1 => __data.Thunk switch {
+                                var __v0 => __v0.Body
+                            }
+                        },
+                        __e0.Right,
+                        __e0.IsLiftedToNull,
+                        __e0.Method
+                    )
+                }
+            ",
+            actual: results.AnalysisResults[0].BodyTree.ToString()
+        );
+    }
+    
+    [Fact]
+    public void Should_work_for_Func1_provided_as_literal() {
+        // This test case is necessary because the call to replace is no longer required (and you cannot rely
+        // on type inferral in the SmallDictionary construction)
+        var results = InterpolationInterceptorGeneratorTestBuilder.Create()
+        .Generate(@"
+            ExpressionOnNone.Interpolate(
+                default(object),
+                x => x.SpliceBody(() => 41) + 1
+            );
+        ");
+        
+        Assert.Equal(1, results.AnalysisResults.Count);
+        CodeGenAssert.CodeEqual(
+            expected: @"
+                (global::System.Linq.Expressions.BinaryExpression)(expression.Body) switch {
+                    var __e0 => global::System.Linq.Expressions.Expression.MakeBinary(
+                        __e0.NodeType,
+                        (global::System.Linq.Expressions.MethodCallExpression)(__e0.Left) switch {
+                            var __e1 => __t0.Coerce(() => 41) switch {
+                                var __v0 => __v0.Body
+                            }
+                        },
+                        __e0.Right,
+                        __e0.IsLiftedToNull,
+                        __e0.Method
+                    )
+                }
+            ",
+            actual: results.AnalysisResults[0].BodyTree.ToString()
+        );
+    }
+
+    [Fact]
     public void Should_work_for_Func2_provided_via_data() {
         var results = InterpolationInterceptorGeneratorTestBuilder.Create()
         .Generate(@"
-            var ownerExpr = ExpressionOn<Owner>.Of(o => o.Name == ""Jon"");
             ExpressionOn<Cat>.Interpolate(
                 new { OwnerPredicate = ExpressionOn<Owner>.Of(o => o.Name == ""Jon"") },
                 (x, c) => x.SpliceBody(c.Owner, x.Data.OwnerPredicate)
---
+++ src/Arborist/test/InterpolateTests.SpliceBody.cs
@@ -0,0 +1,57 @@
+using Arborist.TestFixtures;
+
+namespace Arborist;
+
+public partial class InterpolateTests {
+    [Fact]
+    public void SpliceBody_should_work_for_Func1_provided_via_data() {
+        var interpolated = InterpolationTestOnNone.Interpolate(
+            new { Thunk = ExpressionOnNone.Of(() => 41) },
+            x => x.SpliceBody(x.Data.Thunk) + 1
+        );
+        
+        var expected = Expression.Lambda<Func<int>>(
+            Expression.Add(Expression.Constant(41), Expression.Constant(1))
+        );
+        
+        Assert.Equivalent(expected, interpolated);
+    }
+    
+    [Fact]
+    public void SpliceBody_should_work_for_Func1_provided_as_literal() {
+        var interpolated = ExpressionOnNone.Interpolate(
+            default(object),
+            x => x.SpliceBody(() => 41) + 1
+        );
+        
+        var expected = Expression.Lambda<Func<int>>(
+            Expression.Add(Expression.Constant(41), Expression.Constant(1))
+        );
+        
+        Assert.Equivalent(expected, interpolated);
+    }
+
+    [Fact]
+    public void SpliceBody_should_work_for_Func2_provided_via_data() {
+        var interpolated = InterpolationTestOn<Cat>.Interpolate(
+            new { OwnerPredicate = ExpressionOn<Owner>.Of(o => o.Name == "Jon") },
+            (x, c) => x.SpliceBody(c.Owner, x.Data.OwnerPredicate)
+        );
+        
+        var expected = ExpressionOn<Cat>.Of(c => c.Owner.Name == "Jon");
+        
+        Assert.Equivalent(expected, interpolated);
+    }
+    
+    [Fact]
+    public void SpliceBody_should_work_for_Func2_provided_as_literal() {
+        var interpolated = ExpressionOn<Cat>.Interpolate(
+            default(object),
+            (x, c) => x.SpliceBody(c.Owner, o => o.Name == "Jon")
+        );
+        
+        var expected = ExpressionOn<Cat>.Of(c => c.Owner.Name == "Jon");
+        
+        Assert.Equivalent(expected, interpolated);
+    }
+}

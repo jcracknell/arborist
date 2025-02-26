86b94b18ba8675187e27eb27eb8db8160d6ac524
James Cracknell <james.h.cracknell@gmail.com>
2025-02-26 11:03:30 -0400

    Add test case for interpolated SpliceBody substitutions

--- src/Arborist.Interpolation.InterceptorGenerator/test/SpliceBodyTests.cs
+++ src/Arborist.Interpolation.InterceptorGenerator/test/SpliceBodyTests.cs
@@ -143,4 +143,61 @@ public class SpliceBodyTests {
             actual: results.AnalysisResults[0].BodyTree.ToString()
         );
     }
+
+    [Fact]
+    public void Should_support_interpolation_within_argument_substitutions() {
+        var results = InterpolationInterceptorGeneratorTestBuilder.Create()
+        .Generate(@"
+            var data = new {
+                CatOwner = ExpressionOn<Cat>.Of(c => c.Owner),
+                OwnerPredicate = ExpressionOn<Owner>.Of(o => o.Name == ""Jon"")
+            };
+
+            ExpressionOn<Cat>.Interpolate(data,
+                (x, c) => x.SpliceBody(x.SpliceBody(c, x.Data.CatOwner), x.Data.OwnerPredicate)
+            );
+        ");
+
+        Assert.Equal(1, results.AnalysisResults.Count);
+        CodeGenAssert.CodeEqual(
+            expected: @"
+                (global::System.Linq.Expressions.MethodCallExpression)(expression.Body) switch {
+                    var __e0 => (
+                        (global::System.Linq.Expressions.MethodCallExpression)(__e0.Arguments[0]) switch {
+                            var __e1 => (
+                                __e1.Arguments[0],
+                                (global::System.Linq.Expressions.MemberExpression)(__e1.Arguments[1]) switch {
+                                    var __e2 => __data.CatOwner
+                                }
+                            ) switch {
+                                var (__v1, __v2) => global::Arborist.ExpressionHelper.Replace(
+                                    __v2.Body,
+                                    global::Arborist.Internal.Collections.SmallDictionary.Create(
+                                        new global::System.Collections.Generic.KeyValuePair<global::System.Linq.Expressions.Expression, global::System.Linq.Expressions.Expression>(
+                                            __v2.Parameters[0],
+                                            __v1
+                                        )
+                                    )
+                                )
+                            }
+                        },
+                        (global::System.Linq.Expressions.MemberExpression)(__e0.Arguments[1]) switch {
+                            var __e1 => __data.OwnerPredicate
+                        }
+                    ) switch {
+                        var (__v0, __v3) => global::Arborist.ExpressionHelper.Replace(
+                            __v3.Body,
+                            global::Arborist.Internal.Collections.SmallDictionary.Create(
+                                new global::System.Collections.Generic.KeyValuePair<global::System.Linq.Expressions.Expression, global::System.Linq.Expressions.Expression>(
+                                    __v3.Parameters[0],
+                                    __v0
+                                )
+                            )
+                        )
+                    }
+                }
+            ",
+            actual: results.AnalysisResults[0].BodyTree.ToString()
+        );
+    }
 }

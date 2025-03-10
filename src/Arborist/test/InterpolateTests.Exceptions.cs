5ab3ddf4f752f72beaed480f295322c8eb216182
James Cracknell <james.h.cracknell@gmail.com>
2025-03-10 16:50:11 -0300

    Remove obsolete InterpolateRuntimeFallback overloads

--- src/Arborist/test/InterpolateTests.Exceptions.cs
+++ src/Arborist/test/InterpolateTests.Exceptions.cs
@@ -8,7 +8,7 @@ public partial class InterpolateTests {
     public void Should_throw_InterpolatedParameterEvaluationException() {
         #pragma warning disable ARB003
         Assert.Throws<InterpolatedParameterEvaluationException>(() => {
-            ExpressionOn<Owner>.InterpolateRuntimeFallback(default(object), (x, o) => x.SpliceConstant(o.Name));
+            ExpressionOn<Owner>.Interpolate(default(object), (x, o) => x.SpliceConstant(o.Name));
         });
         #pragma warning restore
     }
@@ -17,21 +17,21 @@ public partial class InterpolateTests {
     public void Should_throw_InterpolatedParameterEvaluationException_for_evaluated_context_reference() {
         #pragma warning disable ARB002
         Assert.Throws<InterpolatedParameterEvaluationException>(() => {
-            ExpressionOn<Owner>.InterpolateRuntimeFallback(default(object), (x, o) => x.SpliceConstant(x));
+            ExpressionOn<Owner>.Interpolate(default(object), (x, o) => x.SpliceConstant(x));
         });
         #pragma warning restore
     }
 
     [Fact]
     public void Should_not_throw_InterpolatedParameterEvaluationException_for_spliced_data() {
-        ExpressionOn<Owner>.InterpolateRuntimeFallback(default(object), (x, o) => x.SpliceConstant(x.Data));
+        ExpressionOn<Owner>.Interpolate(default(object), (x, o) => x.SpliceConstant(x.Data));
     }
 
     [Fact]
     public void Should_throw_InterpolatedParameterEvaluationException_for_evaluated_splice() {
         #pragma warning disable ARB002
         Assert.Throws<InterpolatedParameterEvaluationException>(() => {
-            ExpressionOn<Owner>.InterpolateRuntimeFallback(default(object), (x, o) => x.SpliceConstant(x.SpliceConstant(32)));
+            ExpressionOn<Owner>.Interpolate(default(object), (x, o) => x.SpliceConstant(x.SpliceConstant(32)));
         });
         #pragma warning restore
     }
@@ -40,7 +40,7 @@ public partial class InterpolateTests {
     public void Should_throw_InterpolationContextReferenceException() {
         #pragma warning disable ARB002
         Assert.Throws<InterpolationContextReferenceException>(() => {
-            ExpressionOnNone.InterpolateRuntimeFallback(default(object), x => x.Data == x.SpliceConstant(x.Data));
+            ExpressionOnNone.Interpolate(default(object), x => x.Data == x.SpliceConstant(x.Data));
         });
         #pragma warning restore
     }
@@ -48,7 +48,7 @@ public partial class InterpolateTests {
     [Fact]
     public void Should_not_throw_InterpolationContextReferenceException_for_shadowed_reference() {
         #pragma warning disable ARB001
-        ExpressionOn<Owner>.InterpolateRuntimeFallback(default(object), (x, o) => o.Cats.Single(x => x.Id == 1));
+        ExpressionOn<Owner>.Interpolate(default(object), (x, o) => o.Cats.Single(x => x.Id == 1));
         #pragma warning restore
     }
 
@@ -57,7 +57,7 @@ public partial class InterpolateTests {
         Func<int> notImplemented = () => throw new NotImplementedException();
 
         var thrown = Assert.Throws<SpliceArgumentEvaluationException>(() => {
-            return ExpressionOn<Owner>.InterpolateRuntimeFallback((x, o) => x.SpliceConstant(notImplemented()));
+            return ExpressionOn<Owner>.Interpolate((x, o) => x.SpliceConstant(notImplemented()));
         });
 
         Assert.IsType<NotImplementedException>(thrown.InnerException);

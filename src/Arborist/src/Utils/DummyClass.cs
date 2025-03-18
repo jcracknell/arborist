f16c5d5b8aff2a2082064ec3233446a4d47562fe
James Cracknell <james.h.cracknell@gmail.com>
2025-03-18 08:36:34 -0300

    Add generic versions of Dummy{Class,Struct}

--- src/Arborist/src/Utils/DummyClass.cs
+++ src/Arborist/src/Utils/DummyClass.cs
@@ -6,3 +6,10 @@ namespace Arborist.Utils;
 public sealed class DummyClass {
     private DummyClass() { }
 }
+
+/// <summary>
+/// Meaningless reference type used to disambiguate methods overloaded by type constraints.
+/// </summary>
+public sealed class DummyClass<A> {
+    private DummyClass() { }
+}
--- src/Arborist/src/Utils/DummyStruct.cs
+++ src/Arborist/src/Utils/DummyStruct.cs
@@ -4,3 +4,8 @@ namespace Arborist.Utils;
 /// Meaningless value type used to disambiguate methods overloaded by type constraints.
 /// </summary>
 public readonly struct DummyStruct { }
+
+/// <summary>
+/// Meaningless value type used to disambiguate methods overloaded by type constraints.
+/// </summary>
+public readonly struct DummyStruct<A> { }
